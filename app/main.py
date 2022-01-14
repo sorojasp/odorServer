aafe47cd9d1684b70de3371d8278d1c1806f6bbe
sorojasp <stivenorlandorojaspulido@gmail.com>
2022-01-13 22:21:51 -0500

    implement a post and the way to get data sended by params

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST"])
 def hello():
+    name=request.args.get("name")
+    print(name)
+
     return "Hello, World!"
