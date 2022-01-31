ab04f6fb0a21165bf153384e0f769d7b4042534f
sorojasp <stivenorlandorojaspulido@gmail.com>
2022-01-31 15:23:29 -0500

    service to save concentration is added

--- app/main.py
+++ app/main.py
@@ -3,12 +3,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.server.server import Server
 
+
+
+from datetime import datetime
+
 app = Flask(__name__)
 
 s1=Server(app)
 
 # Import models
+from app.models.ubication import Ubication
 from app.models.gasConcentration import GasConcentration
+
+#import schema
+from app.schemas.gasConcentration import GasConcentrationSchema
+
+#gas concentration schema
+gas_schema=GasConcentrationSchema()
 
 # Get db object
 db=s1.getDatabaseObject()
@@ -18,12 +29,58 @@ db.create_all()
 
 
 
-@app.route("/", methods=["POST"])
-def hello():
 
-    print(request.json['lastname'])
+@app.route("/gasConcentrations", methods=["POST"])
+def post():
 
-    name=request.args.get("name")
-    print(name)
+    """
+        data to probe  NH3=200.38&CO2=10000.38&CH4=100000.99&H2S=100.25&SO2=236.88&T=200.38&H=200.388
 
-    return "Hello, World!"
+        ?NH3=200.38&CO2=10000.38&CH4=100000.99
+        &H2S=100.25&SO2=236.88&T=200.38&H=200.388
+
+        &A=200.38&B=10000.38&C=100000.99&H=4.697540
+        &D=100.25E=236.88&F=20.38&G=20.388&I=-74.114441
+    """
+
+    try:
+
+        #print(request.json['lastname']) get data from body
+
+        #Ubication
+        lat=float(request.args.get("H"))
+        lng=float(request.args.get("I"))
+
+        new_ubication=Ubication(lat,lng)
+
+
+
+        NH3=float(request.args.get("A"))
+        CO2=float(request.args.get("B"))
+        CH4=float(request.args.get("C"))
+        H2S=float(request.args.get("D"))
+        SO2=float(request.args.get("E"))
+        temperature=float(request.args.get("F"))
+        humidity=float(request.args.get("G"))
+
+        dateTime=datetime.now()
+
+        s1.getDatabaseObject().session.add(new_ubication)
+        s1.getDatabaseObject().session.commit()
+
+        print("new ubication:", new_ubication)
+
+
+        new_gas_concentation=GasConcentration(NH3, CO2, CH4, H2S, SO2, temperature, humidity, dateTime, new_ubication.id)
+        s1.getDatabaseObject().session.add(new_gas_concentation)
+        s1.getDatabaseObject().session.commit()
+
+
+
+
+
+        return gas_schema.jsonify(new_gas_concentation)
+
+    except Exception as error:
+        print(error)
+        return "Error!"
---
+++ app/schemas/gasConcentration.py
@@ -0,0 +1,6 @@
+from app.server.server import Server
+
+server=Server()
+class GasConcentrationSchema(server.getMarshmallowObject().Schema):
+    class Meta:
+        fields=('id','NH3','CO2','CH4','H2S','SO2','temperature','humidity','dateTime','ubication_id')
